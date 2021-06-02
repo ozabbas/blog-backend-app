a50a68f4febd46e2e9d100b5d24d5566cc7d6e6d
Peter Jang <peter.x.jang@gmail.com>
2021-06-01 22:05:48 -0500

    Change route namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    post "/users" => "users#create"
-    post "/sessions" => "sessions#create"
+  post "/users" => "api/users#create"
+  post "/sessions" => "api/sessions#create"
 
-    get "/posts" => "posts#index"
-    post "/posts" => "posts#create"
-    get "/posts/:id" => "posts#show"
-    patch "/posts/:id" => "posts#update"
-    delete "/posts/:id" => "posts#destroy"
-  end
+  get "/posts" => "api/posts#index"
+  post "/posts" => "api/posts#create"
+  get "/posts/:id" => "api/posts#show"
+  patch "/posts/:id" => "api/posts#update"
+  delete "/posts/:id" => "api/posts#destroy"
 end
